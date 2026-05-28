020b8660a04186452077d5e41098150e9a032a79
sploshcompetitions <splosh_competitions@icloud.com>
2026-05-28 16:50:14 +0100

    Update smiirl.js

--- api/smiirl.js
+++ api/smiirl.js
@@ -3,9 +3,10 @@ export default async function handler(req, res) {
     const response = await fetch(
       "https://tiktok-scraper7.p.rapidapi.com/user/info?unique_id=splosh_competitions",
       {
+        method: "GET",
         headers: {
-          "X-RapidAPI-Key": "c9ecf7756bmsh5ea9280de2d867dp10b377jsnd5ad668d8f2c",
-          "X-RapidAPI-Host": "tiktok-scraper7.p.rapidapi.com",
+          "x-rapidapi-key": "c9ecf7756bmsh5ea9280de2d867dp10b377jsnd5ad668d8f2c",
+          "x-rapidapi-host": "tiktok-scraper7.p.rapidapi.com",
         },
       }
     );
@@ -18,12 +19,13 @@ export default async function handler(req, res) {
       0;
 
     res.status(200).json({
-      number: followers,
+      number: followers
     });
+
   } catch (error) {
     res.status(500).json({
       number: 0,
-      error: error.message,
+      error: error.message
     });
   }
 }
